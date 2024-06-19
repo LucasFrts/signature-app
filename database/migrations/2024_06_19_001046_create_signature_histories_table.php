1c77d6243682ae9872a3ef500c0b7e19140f657f
Lucas <lmrfreitas9@gmail.com>
2024-06-18 23:09:43 -0300

    fix: signature_histories migration

--- database/migrations/2024_06_19_001046_create_signature_histories_table.php
+++ database/migrations/2024_06_19_001046_create_signature_histories_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('signature_id')->constrained();
             $table->timestamp('last_updated_at');
-            $table->integer('old_plan_id');
-            $table->smallInteger('old_status');
+            $table->integer('last_plan_id');
+            $table->smallInteger('last_status');
         });
     }
 
